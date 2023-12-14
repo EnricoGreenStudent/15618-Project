7d0ef912cc1a301c9d36d7ee0387c394d8300163
Enrico Yidao Green <eygreen@ghc48.ghc.andrew.cmu.edu>
2023-12-14 15:54:21 -0500

    Fix dijkstra

--- src/dijkstra.cpp
+++ src/dijkstra.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <set>
 #include <math.h>
 #ifndef SOLVER_HEADER
 #define SOLVER_HEADER
@@ -14,24 +15,29 @@ class Dijkstra : public SSSPSolver {
   int source;
   int numVertices;
   std::vector<std::vector<edge>> edges;
-  std::vector<std::vector<edge>> incomingEdges;
 
 public:
   void dijkstra(int source, std::vector<std::vector<edge>> &edges, std::vector<float> &distance, std::vector<int> &predecessor) {
     this->source = source;
     this->numVertices = edges.size();
     this->edges = edges;
-    
+    std::set<int> visited;
     distance[source] = 0;
-    std::priority_queue<std::pair<float, int>> pq;
+    std::priority_queue<std::pair<float, int>, std::vector<std::pair<float, int>>, std::greater<std::pair<float, int>>> pq;
     pq.push(std::make_pair(0.0f, source));
     while (!pq.empty()) {
-        // float dist = pq.top().first;
         int u = pq.top().second;
         pq.pop();
+        if(visited.find(u) != visited.end()) {
+          continue;
+        }
+        visited.insert(u);
         // iterate through all edges
         for (edge &e : edges[u]) {
             int v = e.dest;
+            if(visited.count(v) != 0) {
+              continue;
+            }
             int w = e.weight;
             if (distance[u] + w < distance[v]) {
                 distance[v] = distance[u] + w;
@@ -46,27 +52,3 @@ public:
     dijkstra(source, edges, distance, predecessor);
   }
 };
-
-/*int main() {
-  std::vector<std::vector<edge>> edges;
-  std::vector<std::vector<edge>> incomingEdges;
-  int numVertices, numEdges, source;
-  std::cin >> numVertices >> numEdges >> source;
-  edges.resize(numVertices);
-  incomingEdges.resize(numVertices);
-  for (int i = 0; i < numEdges; i++) {
-    int u, v;
-    float w;
-    std::cin >> u >> v >> w;
-    edges[u].push_back(edge{v, w});
-    incomingEdges[v].push_back(edge{u, w});
-  }
-  std::vector<float> distance(numVertices, INFINITY);
-  std::vector<int> predecessor(numVertices, -1);
-  Dijkstra solver;
-  solver.dijkstra(source, edges, distance, predecessor);
-  // print results
-  for (int u = 0; u < numVertices; u++) {
-    std::cout << "vert " << u << " dist " << distance[u] << " pred " << predecessor[u] << "\n";
-  }
-}*/
